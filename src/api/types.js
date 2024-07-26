aab33275d4f765fc37b7eeff51fa27c77d387e85
Alex Luckett <alexgluckett@gmail.com>
2024-07-26 11:31:43 +0100

    Remove unused type

--- src/api/types.js
+++ src/api/types.js
@@ -1,23 +1,8 @@
 /**
- * Form API request types
- * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput }>} RequestFormById
- * @typedef {Request<{ Server: { db: Db }, Params: FormBySlugInput }>} RequestFormBySlug
- * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput, Payload: FormDefinition }>} RequestFormDefinition
- * @typedef {Request<{ Server: { db: Db }, Payload: FormMetadataInput }>} RequestFormMetadataCreate
- * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput, Payload: { force: boolean }}>} RequestRemoveFormById
- * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput, Payload: Partial<FormMetadataInput> }>} RequestFormMetadataUpdateById
- */
-
-/**
  * @template {import('@hapi/hapi').ReqRef} [ReqRef=import('@hapi/hapi').ReqRefDefaults]
  * @typedef {import('@hapi/hapi').Request<ReqRef>} Request
  */
 
 /**
- * @typedef {import('@defra/forms-model').FormByIdInput} FormByIdInput
- * @typedef {import('@defra/forms-model').FormBySlugInput} FormBySlugInput
- * @typedef {import('@defra/forms-model').FormDefinition} FormDefinition
- * @typedef {import('@defra/forms-model').FormMetadataInput} FormMetadataInput
- * @typedef {import('@defra/forms-model').FormMetadataAuthor} FormMetadataAuthor
  * @typedef {import('mongodb').Db} Db
  */
