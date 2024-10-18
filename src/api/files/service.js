06adb9506761cb22b3e42a70be08a36c86499d2b
David Stone <davidjamesstone@gmail.com>
2024-10-18 10:51:21 +0100

    Sonarcloud changes

--- src/api/files/service.js
+++ src/api/files/service.js
@@ -167,7 +167,7 @@ function createCsv(input) {
       input,
       /** @type {Callback} */ function (err, output) {
         if (err) {
-          reject(err)
+          reject(err instanceof Error ? err : new Error('CSV stringify error'))
           return
         }
 
