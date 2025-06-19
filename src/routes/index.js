51da92450376251ba86bc7f52bb277933caf7d5e
Mohammed Khalid <emailmokhld@gmail.com>
2025-06-19 13:27:56 +0100

    Revert "chore: another test"
    This reverts commit dbd4e6df8b45de43910cf8fbc610a4b361a1b28c.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import files from '~/src/routes/files.js'
 import form from '~/src/routes/form.js'
 import health from '~/src/routes/health.js'
-import testError from '~/src/routes/test-error.js'
 
-export default [health, files, form, testError].flat()
+export default [health, files, form].flat()
