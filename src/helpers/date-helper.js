c52b536f00a61257b7945cdb34e98c15b8fa78ac
David Stone <davidjamesstone@gmail.com>
2025-11-27 08:47:42 +0000

    Fix addMonths

--- src/helpers/date-helper.js
+++ src/helpers/date-helper.js
@@ -16,6 +16,6 @@ export function addDays(date, days) {
  */
 export function addMonths(date, months) {
   const result = new Date(date)
-  result.setDate(result.getMonth() + months)
+  result.setMonth(result.getMonth() + months)
   return result
 }
