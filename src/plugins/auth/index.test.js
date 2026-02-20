15b579824b418d5a216016891d28f3f9bdcb8f65
David Stone <davidjamesstone@gmail.com>
2026-02-20 12:52:10 +0000

    Fix test

--- src/plugins/auth/index.test.js
+++ src/plugins/auth/index.test.js
@@ -149,7 +149,7 @@ describe('Auth plugin', () => {
       })
     })
 
-    test('Testing validateAuth with a invalid JSON groups object artifact (OIDC mock server) returns isValid: false', async () => {
+    test('Testing validateAuth with a invalid JSON groups object artifact returns isValid: false', async () => {
       const artifacts = buildArtifactStub()
 
       const res = await validateAuth({
@@ -158,8 +158,8 @@ describe('Auth plugin', () => {
           ...artifacts.decoded,
           payload: {
             ...artifacts.decoded.payload,
-            // @ts-expect-error - OIDC test invalid JSON
-            groups: '{}'
+            // @ts-expect-error - test invalid data
+            groups: {}
           }
         }
       })
