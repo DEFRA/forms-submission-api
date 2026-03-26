933e9aa3c665e22718727b516ae9235241c62d15
David Stone <davidjamesstone@gmail.com>
2026-03-26 11:54:22 +0000

    Move getSubmissionRecordByReference into new submission routes file

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,4 @@
 import { Scopes, idSchema } from '@defra/forms-model'
-import Boom from '@hapi/boom'
 import Joi from 'joi'
 
 import {
@@ -11,12 +10,10 @@ import {
   dqlSchema,
   generateFeedbackSubmissionsFileResponseSchema,
   generateFormSubmissionsFileResponseSchema,
-  getSubmissionByReferenceResponseSchema,
   magicLinkSchema,
   receiptHandleSchema,
   resetSaveAndExitLinkResponseSchema
 } from '~/src/models/form.js'
-import { getSubmissionRecordByReference } from '~/src/repositories/submission-repository.js'
 import { resetSaveAndExitLink } from '~/src/services/save-and-exit-service.js'
 import {
   generateFeedbackSubmissionsFileForAll,
@@ -134,46 +131,6 @@ export default [
       response: {
         status: {
           200: generateFeedbackSubmissionsFileResponseSchema
-        }
-      }
-    }
-  }),
-
-  /**
-   * @satisfies {ServerRoute<GetSubmissionByReference>}
-   */
-  ({
-    method: 'GET',
-    path: '/submission/{referenceNumber}',
-    async handler(request) {
-      const { params } = request
-      const { referenceNumber } = params
-
-      const record = await getSubmissionRecordByReference(referenceNumber)
-
-      if (!record) {
-        return Boom.notFound(
-          `Submission record with reference ${referenceNumber} was not found`
-        )
-      }
-
-      return record
-    },
-    options: {
-      tags: ['api'],
-      auth: {
-        scope: [`+${Scopes.FormRead}`]
-      },
-      validate: {
-        params: Joi.object()
-          .keys({
-            referenceNumber: Joi.string().required()
-          })
-          .label('getSubmissionByReferenceParams')
-      },
-      response: {
-        status: {
-          200: getSubmissionByReferenceResponseSchema
         }
       }
     }
