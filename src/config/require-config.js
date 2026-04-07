faab3e79760a77394afe66112e7dd8383583084d
Parvesh M <parveshh@gmail.com>
2026-04-07 11:23:14 +0100

    feat: removal of default env variables

--- src/config/require-config.js
+++ src/config/require-config.js
@@ -5,7 +5,7 @@
  * @returns {NonNullable<T>}
  */
 export function requireConfig(value, key) {
-  if (value == null || value === '') {
+  if (value == null || (typeof value === 'string' && value === '')) {
     throw new Error(`Missing required config: ${key}`)
   }
 
