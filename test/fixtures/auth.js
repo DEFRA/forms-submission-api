32dec53674a711c4ef1570814525892bd8551cee
David Stone <davidjamesstone@gmail.com>
2026-05-05 14:18:11 +0100

    Allow GET submission route to be called by AD only authenticated users (#510)
    * Allow GET submission route to be called by AD only authenticated users
    
    * Remove scp key from authAD mock

--- test/fixtures/auth.js
+++ test/fixtures/auth.js
@@ -61,6 +61,25 @@ export const authAdmin = {
   }
 }
 
+export const authAD = {
+  strategy: 'azure-oidc-token',
+  credentials: {
+    user: {
+      aud: 'api://2bf2d9fd-fe1e-47eb-9821-b6f6cd3ceba1',
+      iss: 'https://sts.windows.net/5b504113-6b64-43f2-ade9-242e05780008/',
+      family_name: 'Chase',
+      given_name: 'Enrique',
+      groups: ['7049296f-2156-4d61-8ac3-349276438ef9'],
+      name: 'Enrique Chase (Defra)',
+      scp: '',
+      sub: 'hgtL_1p2Me5JkBB6JeB20PyU3YDuP9PjEZwi7m1QGng',
+      oid: '86758ba9-92e7-4287-9751-7705e449688e',
+      roles: []
+    },
+    scope: []
+  }
+}
+
 export const appAuth = {
   strategy: 'cognito-access-token',
   credentials: {
