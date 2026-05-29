1358b541456cfdad8e1602e2136dd9eb5b0d6324
David Stone <davidjamesstone@gmail.com>
2026-05-29 14:44:28 +0100

    Add user helper test

---
+++ src/helpers/user.test.js
@@ -0,0 +1,29 @@
+import { getUserEmail } from '~/src/helpers/user.js'
+import { authAdmin } from '~/test/fixtures/auth.js'
+
+describe('user-helper', () => {
+  test('returns email if preferred_username is missing', () => {
+    expect(getUserEmail(authAdmin)).toBe('enrique.chase@defra.gov.uk')
+  })
+
+  test('throw if preferred_username is missing', () => {
+    const err = new Error('User email not found')
+    const authWithoutEmail = /** @type {RequestAuth<UserCredentials>} */ (
+      /** @type {unknown} */ ({
+        ...authAdmin,
+        credentials: {
+          ...authAdmin.credentials,
+          user: {
+            ...authAdmin.credentials.user,
+            preferred_username: undefined // set preferred_username to test error handling
+          }
+        }
+      })
+    )
+    expect(() => getUserEmail(authWithoutEmail)).toThrow(err)
+  })
+})
+
+/**
+ * @import { RequestAuth, UserCredentials, AppCredentials } from '@hapi/hapi'
+ */
