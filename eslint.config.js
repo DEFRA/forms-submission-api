fdce578570772e9fe2e70472be01359111916c10
Alex Luckett <alex@nodepoint.co.uk>
2026-06-05 15:22:03 +0100

    chore: upgrade eslint to v10 and related plugins

--- eslint.config.js
+++ eslint.config.js
@@ -125,6 +125,10 @@ export default defineConfig([
         }
       ],
 
+      // Disable jsdoc v63 new rules that produce too many warnings in this codebase
+      'jsdoc/reject-any-type': 'off',
+      'jsdoc/reject-function-type': 'off',
+
       // JSDoc @param types are mandatory for JavaScript
       'jsdoc/require-param-description': 'off',
       'jsdoc/require-param-type': 'error',
