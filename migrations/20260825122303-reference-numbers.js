1454a374717ef86291cfd7dfa3d99e05e17a4c14
David Stone <davidjamesstone@gmail.com>
2026-09-04 16:15:18 +0100

    Enhance migration logging for reference numbers collection (#560)

--- migrations/20260825122303-reference-numbers.js
+++ migrations/20260825122303-reference-numbers.js
@@ -10,7 +10,7 @@ export const REFERENCE_NUMBERS_COLLECTION_NAME = 'reference-numbers'
  */
 export const up = async (db) => {
   console.log(
-    'Reading submission records and inserting existing reference numbers'
+    '[REF-MIG] Reading submission records and inserting existing reference numbers'
   )
 
   const submissionsColl = /** @type {Collection<FormSubmissionDocument>} */ (
@@ -22,10 +22,22 @@ export const up = async (db) => {
       db.collection(REFERENCE_NUMBERS_COLLECTION_NAME)
     )
 
+  console.log(
+    '[REF-MIG] Adding unique index to the new reference-numbers collection'
+  )
+
   // Add unique index on the `referenceNumber` field to the `reference-numbers` collection
   await referenceNumbersColl.createIndex(
     { referenceNumber: 1 },
     { unique: true }
+  )
+
+  console.log(
+    '[REF-MIG] Added unique index to the new reference-numbers collection'
+  )
+
+  console.log(
+    '[REF-MIG] Adding records into reference-numbers collection from the existing submissions collection'
   )
 
   let counter = 0
@@ -38,10 +50,26 @@ export const up = async (db) => {
       submissionId: record._id
     })
     ++counter
+
+    if (counter % 5000 === 0) {
+      console.log(`[REF-MIG] Counter ${counter} records processed`)
+    }
   }
+
+  console.log(
+    `[REF-MIG] Added ${counter} records into reference-numbers collection from the existing submissions collection`
+  )
+
+  console.log(
+    `[REF-MIG] Dropping the non-unique index on the meta.referenceNumber field in the submissions collection`
+  )
 
   // Drop the (non-unique) index on the `meta.referenceNumber` field in the `submissions` collection
   await submissionsColl.dropIndex('meta.referenceNumber_1')
+
+  console.log(
+    `[REF-MIG] Creating unique index on the meta.referenceNumber field in the submissions collection`
+  )
 
   // Re-add unique index on the `meta.referenceNumber` field to the `submissions` collection
   await submissionsColl.createIndex(
@@ -49,7 +77,9 @@ export const up = async (db) => {
     { unique: true }
   )
 
-  console.log(`Inserted ${counter} reference numbers`)
+  console.log(
+    `[REF-MIG] Finished migration of reference numbers to the new collection`
+  )
 }
 
 /**
